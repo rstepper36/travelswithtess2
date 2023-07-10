0316983bbfbf6b2871b918914158456878c30ac9
Roger Stepper <rstepper36@gmail.com>
2023-07-09 22:38:43 -0700

    corrected errors in the config.js file

--- config/config.js
+++ config/config.js
@@ -1,5 +1,29 @@
-const Sequelize = require('sequelize');
+require('dotenv').config();
+const url = require('url');
 
-module.exports = function() {
-  return new Sequelize(process.env.CLEARDB_DATABASE_URL);
+if (process.env.CLEARDB_DATABASE_URL) {
+  // Heroku ClearDB configuration
+  const dbUrlParts = url.parse(process.env.CLEARDB_DATABASE_URL);
+  const [username, password] = dbUrlParts.auth.split(':');
+
+  module.exports = {
+    production: {
+      username,
+      password,
+      database: dbUrlParts.pathname.substr(1),
+      host: dbUrlParts.hostname,
+      dialect: 'mysql',
+    },
+  };
+} else {
+  // local configuration
+  module.exports = {
+    development: {
+      username: process.env.DB_USER,
+      password: process.env.DB_PASSWORD,
+      database: process.env.DB_DATABASE,
+      host: process.env.DB_HOST,
+      dialect: 'mysql',
+    },
+  };
 }
