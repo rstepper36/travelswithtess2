6359f4964632367e179f8c68800238ef1ca838a8
Roger Stepper <rstepper36@gmail.com>
2023-07-09 22:45:35 -0700

    Tweaked the config.js file again

--- config/config.js
+++ config/config.js
@@ -1,32 +1,41 @@
 require('dotenv').config();
 const url = require('url');
 
-let config;
+let config = {
+  development: {
+    username: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_DATABASE,
+    host: process.env.DB_HOST,
+    dialect: 'mysql',
+  },
+  test: {
+    username: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_DATABASE,
+    host: process.env.DB_HOST,
+    dialect: 'mysql',
+  },
+  production: {
+    username: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_DATABASE,
+    host: process.env.DB_HOST,
+    dialect: 'mysql',
+  },
+};
 
 if (process.env.CLEARDB_DATABASE_URL) {
   // Heroku ClearDB configuration
   const dbUrlParts = url.parse(process.env.CLEARDB_DATABASE_URL);
   const [username, password] = dbUrlParts.auth.split(':');
 
-  config = {
-    production: {
-      username,
-      password,
-      database: dbUrlParts.pathname.substr(1),
-      host: dbUrlParts.hostname,
-      dialect: 'mysql',
-    },
-  };
-} else {
-  // local configuration
-  config = {
-    development: {
-      username: process.env.DB_USER,
-      password: process.env.DB_PASSWORD,
-      database: process.env.DB_DATABASE,
-      host: process.env.DB_HOST,
-      dialect: 'mysql',
-    },
+  config.production = {
+    username,
+    password,
+    database: dbUrlParts.pathname.substr(1),
+    host: dbUrlParts.hostname,
+    dialect: 'mysql',
   };
 }
 
