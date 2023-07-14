a98d5499f6c4c7adfc34776f014fbc92a798f949
Roger Stepper <rstepper36@gmail.com>
2023-07-13 22:50:03 -0700

    Added Edit capability

--- routes/posts.js
+++ routes/posts.js
@@ -15,6 +15,7 @@ module.exports = (upload) => {
   });
   
 
+  // Route for showing all posts
   router.get('/', (req, res) => {
     models.Post.findAll({
         include: [{
@@ -26,12 +27,13 @@ module.exports = (upload) => {
         if (posts.length === 0) {
             return res.render('no-posts'); // no posts render no-posts view
         }
-        res.render('posts', { posts: posts }); // render posts view
+        res.render('posts', { posts: posts, user: req.session.user }); // render posts view
     })
     .catch(err => {
       next(err);
     });
-  });
+});
+
 
   // Route for creating a post and uploading an image
   router.post('/create', upload.single('imageURL'), (req, res, next) => {
@@ -56,84 +58,83 @@ module.exports = (upload) => {
   });
 });
 
-
-
-  router.post('/:id', (req, res) => {
-    const { content } = req.body;
-    models.Comment.create({
-      content: content,
-      PostId: req.params.id,
-      UserId: req.session.user.id
-    }).then(comment => {
-      res.redirect('/posts/' + req.params.id);
-    });
+  // Route for creating a comment
+router.post('/:id', (req, res) => {
+  const { content } = req.body;
+  models.Comment.create({
+    content: content,
+    PostId: req.params.id,
+    UserId: req.session.user.id
+  }).then(comment => {
+    res.redirect('/posts/' + req.params.id);
   });
+});
 
-  router.get('/:id', (req, res) => {
-    models.Post.findByPk(req.params.id, {
-      include: [{
-        model: models.Comment,
-        include: [models.User]
-      }, models.User]
-    }).then(post => {
+router.get('/:id', (req, res) => {
+  models.Post.findByPk(req.params.id, {
+    include: [{
+      model: models.Comment,
+      include: [models.User]
+    }, models.User]
+  }).then(post => {
+    if (!post) {
+      return res.status(404).send('Post not found');
+    }
+    res.render('post', { post: post });
+  }).catch(err => {
+    next(err);
+  });
+});
+
+// Route for showing the edit form
+router.get('/:id/edit', (req, res, next) => {
+  if (!req.session.user) {
+    return res.redirect('/users/login');
+  }
+  models.Post.findByPk(req.params.id)
+    .then(post => {
       if (!post) {
         return res.status(404).send('Post not found');
       }
-      res.render('post', { post: post });
-    }).catch(err => {
+      if (req.session.user.id !== post.UserId) {
+        return res.status(403).send('You do not have permission to edit this post');
+      }
+      res.render('edit', { post: post });
+    })
+    .catch(err => {
       next(err);
     });
-  });
+});
+  
+// Route for updating a post
+router.put('/:id', upload.single('imageURL'), (req, res, next) => {
+  if (!req.session.user) {
+    return res.redirect('/users/login');
+  }
+  const { title, content } = req.body;
+  const imageURL = req.file ? req.file.location : null;
 
-    // Route for showing the edit form
-    router.get('/:id/edit', (req, res, next) => {
-      if (!req.session.user) {
-        return res.redirect('/users/login');
+  models.Post.findByPk(req.params.id)
+    .then(post => {
+      if (!post) {
+        return res.status(404).send('Post not found');
       }
-      models.Post.findByPk(req.params.id)
-        .then(post => {
-          if (!post) {
-            return res.status(404).send('Post not found');
-          }
-          if (req.session.user.id !== post.UserId) {
-            return res.status(403).send('You do not have permission to edit this post');
-          }
-          res.render('edit', { post: post });
-        })
-        .catch(err => {
-          next(err);
-        });
-    });
-  
-    // Route for updating a post
-    router.put('/:id', upload.single('imageURL'), (req, res, next) => {
-      if (!req.session.user) {
-        return res.redirect('/users/login');
+      if (req.session.user.id !== post.UserId) {
+        return res.status(403).send('You do not have permission to edit this post');
       }
-      const { title, content } = req.body;
-      const imageURL = req.file ? req.file.location : null;
-  
-      models.Post.findByPk(req.params.id)
-        .then(post => {
-          if (!post) {
-            return res.status(404).send('Post not found');
-          }
-          if (req.session.user.id !== post.UserId) {
-            return res.status(403).send('You do not have permission to edit this post');
-          }
-          return post.update({
-            title: title,
-            content: content,
-            imageURL: imageURL || post.imageURL // Keep the old image if a new one was not uploaded
-          });
-        })
-        .then(post => {
-          res.redirect('/posts/' + post.id);
-        })
-        .catch(err => {
-          next(err);
-        });
+      return post.update({
+        title: title,
+        content: content,
+        imageURL: imageURL || post.imageURL // Keep the old image if a new one was not uploaded
+      });
+    })
+    .then(post => {
+      res.redirect('/posts/' + post.id);
+    })
+    .catch(err => {
+      next(err);
     });
+});
   
 
   return router;
