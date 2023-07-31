3f90061a148b3ae764a739bac77f9e3f6a608870
Roger Stepper <rstepper36@gmail.com>
2023-07-30 18:09:03 -0700

    added view and route changes

--- models/post.js
+++ models/post.js
@@ -2,13 +2,13 @@ module.exports = (sequelize, DataTypes) => {
   const Post = sequelize.define('Post', {
     title: DataTypes.STRING,
     content: DataTypes.TEXT,
-    UserId: DataTypes.INTEGER,
+     UserId: DataTypes.INTEGER,
+     imageURL: DataTypes.STRING // new image field attribute
   });
 
   Post.associate = function(models) {
     Post.belongsTo(models.User);
     Post.hasMany(models.Comment);
-    Post.hasMany(models.Image); // This line establishes a relationship between Post and Image models
   };
 
   return Post;
